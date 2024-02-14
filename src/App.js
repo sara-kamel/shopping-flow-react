c7ac73aceaad16041984ccc36cfb718fb43b4398
Sara Kamel <sarakamel591@gmail.com>
2024-02-14 10:09:01 -0800

    Update package.json file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-
-import { useState } from 'react';
-import './style.css';
-import Container from 'react-bootstrap/Container';
-import Products from './Products';
-import Navbar from 'react-bootstrap/Navbar';
-import { Link, Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import Cart from './Cart';
-import ProductDetails from './ProductDetails';
-import { countQuantity } from './helper';
+import { useState } from "react";
+import "./style.css";
+import Container from "react-bootstrap/Container";
+import Products from "./Products";
+import Navbar from "react-bootstrap/Navbar";
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Cart from "./Cart";
+import ProductDetails from "./ProductDetails";
+import { countQuantity } from "./helper";
 
 export default function App() {
   const [cartCount, setCartCount] = useState(() => {
-    const cartList = localStorage.getItem('cartlist');
+    const cartList = localStorage.getItem("cartlist");
     if (cartList) {
       return countQuantity(JSON.parse(cartList));
     } else {
