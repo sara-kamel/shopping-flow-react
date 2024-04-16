7d7851896e3fdbd1526466c722c7d3c8a85f7680
Sara Kamel <sarakamel591@gmail.com>
2024-04-15 17:37:23 -0700

    change the UI design for Cart component

--- src/Cart.js
+++ src/Cart.js
@@ -1,8 +1,11 @@
-
 import { useNavigate } from "react-router-dom";
-import { Button, Card, Form } from "react-bootstrap";
 import { countTotal, updateCartQuantity, countQuantity } from "./helper";
 import useLocalStorage from "./useLocalStorage";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { Grid } from "@mui/material";
+import { ButtonGroup, Stack } from "react-bootstrap";
 
 export default function Cart({ onChangeItemsCount }) {
   const navigate = useNavigate();
@@ -20,12 +23,20 @@ export default function Cart({ onChangeItemsCount }) {
   const totalPrice = total + tax;
 
   return (
-    <section className="cart-container">
+    <>
       <h1>Your Cart</h1>
-      <article>
+      <Stack direction="horizontal" gap={3}>
         {cartList.map((product) => (
-          <Card className="cart-product" key={product.id}>
-            <Card.Img variant="top" src={product.image} />
+          <Card
+            key={product.id}
+            style={{ width: "18rem", height: "35rem" }}
+            className="p-2"
+          >
+            <Card.Img
+              variant="top"
+              src={product.image}
+              style={{ height: "15rem" }}
+            />
             <Card.Body>
               <Card.Title
                 title="show product"
@@ -34,7 +45,7 @@ export default function Cart({ onChangeItemsCount }) {
                   navigate(`/products/${product.id}`);
                 }}
               >
-                {product.title}{" "}
+                {product.title}
               </Card.Title>
               <h1>{product.price}</h1>
             </Card.Body>
@@ -54,33 +65,30 @@ export default function Cart({ onChangeItemsCount }) {
               }}
             />
             <Button
-              variant="outline-info"
+              variant="danger"
               onClick={() => {
                 setCartList(cartList.filter((p) => p.id !== product.id));
                 onChangeItemsCount(countQuantity(cartList) - product.quantity);
               }}
             >
-              &#127485;
+              Delete From Cart
             </Button>
           </Card>
         ))}
-      </article>
-      <br />
-      <hr />
+      </Stack>
 
+      <hr />
       <div>
         <h5>price: {total.toFixed(2)}</h5>
         <h5>tax: {tax.toFixed(2)}</h5>
         <h5> total: {totalPrice.toFixed(2)}</h5>
-
         <Button variant="outline-info" onClick={() => navigate("/checkout")}>
           Check Out
         </Button>
-        <br />
         <Button variant="outline-info" onClick={() => navigate("/products")}>
           go to shopping list
         </Button>
       </div>
-    </section>
+    </>
   );
 }
